50c7ca512cad786e90ff7e2b9df0079552550689
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-07-13 00:35:49 +0100

    Force download of macos_64 root

--- recipe/build_bdsim.sh
+++ recipe/build_bdsim.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -eux
 
+tar zxf root_v6.36.00.macos-14.7-x86_64-clang160.tar.gz
+source ./root/bin/thisroot.sh 
+
 mkdir bdsim-build
 cd bdsim-build
 
 cmake $CMAKE_ARGS -DCMAKE_PREFIX_PATH=${PREFIX}/lib/cmake/Geant4/ \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+
 
 make "-j${CPU_COUNT}" ${VERBOSE_CM:-}
 make install "-j${CPU_COUNT}"
