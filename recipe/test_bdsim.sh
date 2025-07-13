3474d580467177fcd4110b04806a1c7bd8989df3
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-07-13 13:06:58 +0100

    Skipping test on macosx_arm64

--- recipe/test_bdsim.sh
+++ recipe/test_bdsim.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-bdsim --help
+if [[ "$target_platform" == "osx-arm64" ]]; then
+    echo "Skipping tests on osx-arm64"
+    exit 0
+else
+    bdsim --help
+    bdsim --file=${RECIPE_DIR}/drift.gmad --batch --ngenerate=100 
+fi
 
-bdsim --file=${RECIPE_DIR}/drift.gmad --batch --ngenerate=100 
+
