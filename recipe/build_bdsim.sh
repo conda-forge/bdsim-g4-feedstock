bd59d464ad313735bf19b8d093bb58339916b377
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-19 21:19:14 +0200

    HDF5 build

--- recipe/build_bdsim.sh
+++ recipe/build_bdsim.sh
@@ -6,6 +6,7 @@ cd bdsim-build
 
 cmake $CMAKE_ARGS -DCMAKE_PREFIX_PATH=${PREFIX}/lib/cmake/Geant4/ \
       -DUSE_HEPMC3=ON \
+      -DUSE_PLASMA_HDF5=ON \
       "${SRC_DIR}"
 
 make "-j${CPU_COUNT}" ${VERBOSE_CM:-}
