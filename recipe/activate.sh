e69fba3062160728b59d05a76fd22839999a4b7c
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-20 02:09:17 +0200

    Add activate recipies

---
+++ recipe/activate.sh
@@ -0,0 +1,2 @@
+export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${CONDA_PREFIX}/include/bdsim/:$BDSIM/include/bdsim/analysis/:${CONDA_PREFIX}/include/bdsim/parser/
+export PYTHONPATH=${PYTHON_PATH}:${CONDA_PREFIX}/lib/python/
